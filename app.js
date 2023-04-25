80f8f9d3f67a41462140b27671e9c6baa698149a
Mehin Melikova <mhmelikova@gmail.com>
2023-04-25 14:52:36 +0400

    openweather api added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000
 
 app.set("view engine", "ejs")
+app.use('/public', express.static('public'));
 
 app.get('/', async (req, res)=>{
     let response = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=40.3777&lon=49.8920&units=metric&appid=9161e24f7473156ec9b6612da2fca736");
