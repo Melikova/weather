7ddc1a8ee0297a218a03921fdee47d0c940a7216
Mehin Melikova <mhmelikova@gmail.com>
2023-04-25 16:13:09 +0400

    css added

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ app.use('/public', express.static('public'));
 app.get('/', async (req, res)=>{
     let response = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=40.3777&lon=49.8920&units=metric&appid=9161e24f7473156ec9b6612da2fca736");
     data = await response.json();
+    let months= ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    let d = new Date(data.dt * 1000);
+    data.date=[d.getDate(), months[d.getMonth()], d.getFullYear(), days[d.getDay()]];
+    data.hour=d.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
     // res.send(data)
     res.render('index', data)
 })
